173a25d60cc7b7c00e9b9c0aaa3cd8708cecea77
Marvin Orellana <notkooru@gmail.com>
2023-02-10 09:34:21 -0600

    Updated Account class

--- main.py
+++ main.py
@@ -3,6 +3,21 @@
 class Account:
     def setUsername(self, username):
         self.username = username
+
+    def getUsername(self): 
+        return self.username
     
     def setPassword(self, password):
         self.password = password
+    
+    def getPassword(self):
+        return self.password
+
+test = Account()
+
+test.setPassword("123")
+test.setUsername("username")
+print(test.getUsername())
+print(test.getPassword())
+test.setUsername("desolacion")
+print(test.getUsername())
